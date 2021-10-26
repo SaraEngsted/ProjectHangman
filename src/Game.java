0315caab312d19f8a1fde1fe7849dd8be7d2e2b5
SaraEngsted <sara.andreasen@live.dk>
2021-10-26 15:00:07 +0200

    2/3 finish

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,26 @@
-public class Game {
+import java.util.ArrayList;
 
+public class Game extends FileReader {
+
+    static ArrayList<Character> guesses = new ArrayList<Character>();
+
+    public Game(String chosenWord) {
+        super();
+    }
+
+
+    public static void changeLettersToLines(String words) {
+        for (int i = 0; i < words.length(); i++) {
+        if (guesses.contains(words));
+            System.out.print(words.replaceAll(words, ("_")));
+
+        }
+    }
+
+    public static void hangman() {
+
+    }
 
 
 }
+
--- src/HangmanMain.java
+++ src/HangmanMain.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class HangmanMain {
 
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("________________________");
+        System.out.println("😈 WELCOME TO HANGMAN 😈");
+        System.out.println("________________________");
+        System.out.println("Press ENTER to start game");
+        scanner.nextLine();
+        System.out.println("Your word is down below");
+        System.out.println("⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇");
+
+        FileReader.getWords();
+        String chosenWord = FileReader.getRandomWord();
+
+        Game hangman = new Game(chosenWord);
+        hangman.changeLettersToLines(chosenWord);
+    }
 
 
 }
